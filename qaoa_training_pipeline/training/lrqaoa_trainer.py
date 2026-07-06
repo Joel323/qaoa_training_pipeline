72538d3c182f55cfd068bc8b089e6c74b6ce3426
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-07-06 14:49:26 +0200

    Add default parameters to LRQAOA

--- qaoa_training_pipeline/training/lrqaoa_trainer.py
+++ qaoa_training_pipeline/training/lrqaoa_trainer.py
@@ -8,10 +8,14 @@
 
 """A class that implements the Linear Ramp QAOA Protocol"""
 
+from qiskit.quantum_info import SparsePauliOp
+from qiskit import QuantumCircuit
 
+from qaoa_training_pipeline.framework.param_result import ParamResult
 from qaoa_training_pipeline.training.functions import TQATrainerFunction
 from qaoa_training_pipeline.training.scipy_trainer import ScipyTrainer
 from qaoa_training_pipeline.evaluation.base_evaluator import BaseEvaluator
+from qaoa_training_pipeline.evaluation import EVALUATORS
 
 
 class LRQAOATrainer(ScipyTrainer):
@@ -29,8 +33,8 @@ class LRQAOATrainer(ScipyTrainer):
 
     def __init__(
         self,
-        reps: int,
-        evaluator: BaseEvaluator,
+        reps: int | None = None,
+        evaluator: BaseEvaluator | None = None,
         minimize_args: dict[str, object] | None = None,
         energy_minimization: bool = False,
     ):
@@ -49,4 +53,38 @@ class LRQAOATrainer(ScipyTrainer):
             minimize_args,
             energy_minimization,
             TQATrainerFunction(reps=reps, tqa_schedule_method="lr_schedule"),
+        )
+
+    def provide_params(
+        self,
+        cost_op: SparsePauliOp | None = None,
+        mixer: QuantumCircuit | None = None,
+        initial_state: QuantumCircuit | None = None,
+        ansatz_circuit: QuantumCircuit | None = None,
+        params0: list[float] | None = None,
+    ) -> ParamResult:
+        """Adds default params0 value for cases where the user does not input an initial
+        value for the TQA schedule.
+        """
+
+        params0 = params0 or [0.5, 0.5]
+        return super().provide_params(
+            cost_op=cost_op,
+            mixer=mixer,
+            initial_state=initial_state,
+            ansatz_circuit=ansatz_circuit,
+            params0=params0,
+        )
+
+    @classmethod
+    def from_config(cls, config: dict) -> "LRQAOATrainer":
+        """Create a scipy trainer based on a config."""
+
+        evaluator_cls = EVALUATORS[config["evaluator"]]
+
+        return cls(
+            evaluator=evaluator_cls.from_config(config["evaluator_init"]),
+            reps=config.get("reps"),
+            minimize_args=config.get("minimize_args", None),
+            energy_minimization=config.get("energy_minimization", False),
         )
