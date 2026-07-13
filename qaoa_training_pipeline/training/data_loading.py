bf0d126f6041d86069c99c56fc5492c22486f213
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-07-13 15:12:43 +0200

    Revert LoadFromJson nested property

--- qaoa_training_pipeline/training/data_loading.py
+++ qaoa_training_pipeline/training/data_loading.py
@@ -62,71 +62,15 @@ class LoadFromJson(BaseDataLoader):
     converted to the tuple of features `(2.0, 6.0, 9.0, 3.0, -0.5, 0.6)`.
     """
 
-    def __init__(self, file_name: str, nested: bool = False):
+    def __init__(self, file_name: str):
         """Setup the loader by specifying the file from which to load."""
         self._file_name = file_name
-        self._nested = nested
 
     def __call__(self):
         """Load the data."""
         with open(self._file_name, "r") as fin:
             json_data = json.load(fin)
 
-        if self._nested:
-            return self._load_nested(json_data)
-        else:
-            return self._load_flat(json_data)
-
-    def _load_nested(self, json_data: dict) -> dict:
-        """Load nested structure and flatten to tuple keys.
-
-        Transforms: {degree: {reps: {beta, gamma, AR}}}
-        To: {(reps, degree): {qaoa_angles: [beta + gamma]}}
-
-        Note: This transformation on the data needs to be used when using
-        the fixed angles conjecture method
-        """
-        data = dict()
-
-        for outer_key, inner_dict in json_data.items():
-            # Skip non-dict entries (like "description")
-            if not isinstance(inner_dict, dict):
-                continue
-
-            # Convert outer key to float
-            try:
-                outer_val = float(outer_key)
-            except ValueError:
-                # Skip keys that can't be converted (like "description")
-                continue
-
-            for inner_key, angles_dict in inner_dict.items():
-                # Convert inner key to int (for reps/qaoa_depth)
-                inner_val = int(inner_key)
-
-                # Create tuple key: (qaoa_depth, degree)
-                tuple_key = (inner_val, outer_val)
-
-                # Transform angles to TransferTrainer format
-                if "beta" in angles_dict and "gamma" in angles_dict:
-                    qaoa_angles = angles_dict["beta"] + angles_dict["gamma"]
-                    data[tuple_key] = {"qaoa_angles": qaoa_angles}
-
-                    # Preserve additional metadata if present
-                    if "AR" in angles_dict:
-                        data[tuple_key]["metadata"] = angles_dict["AR"]
-                else:
-                    # If not in fixed-angle format, store as-is
-                    data[tuple_key] = angles_dict
-
-        return data
-
-    def _load_flat(self, json_data):
-        """Load the features from a given database to tuple keys
-
-        Note: This is needed to use parameter transfer from a given dataset of pre-trained
-        QAOA angles to a given problem instance.
-        """
         data = dict()
         for key, val in json_data.items():
             tuple_key = tuple(float(val) for val in key.split(","))
@@ -142,7 +86,7 @@ class LoadFromJson(BaseDataLoader):
     @classmethod
     def from_config(cls, config) -> "LoadFromJson":
         """Setup the loader from a config file."""
-        return cls(config["file_name"], config["nested"])
+        return cls(config["file_name"])
 
 
 DATA_LOADERS = {
