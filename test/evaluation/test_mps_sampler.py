f457354106928ad1488daf527ef3f8b9af02d64a
Joel Jurado Diaz <jurado.diaz.joel@gmail.com>
2026-07-23 11:54:21 +0200

    Change tolerance for evaluation test MPS Sampler

--- test/evaluation/test_mps_sampler.py
+++ test/evaluation/test_mps_sampler.py
@@ -50,7 +50,7 @@ class TestSampleEvaluator(TestCase):
                 evaluator = SampleEvaluator(shots=10000, chi=32)
                 energy1 = evaluator.evaluate(cost_op, params=angles)
                 energy2 = self.qiskit_circuit_simulation(cost_op, angles)
-                self.assertTrue(abs(energy1 - energy2) < 0.05)
+                self.assertTrue(abs(energy1 - energy2) < 0.08)
 
     def test_custom_ansatz(self):
         """Test that we can construct the ansatz from a different operator."""
